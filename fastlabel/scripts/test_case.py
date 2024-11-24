adf40add784d2625c51f89ad5c9c9d629b66d9a7
lgactna <lgonzalesna@gmail.com>
2024-11-23 22:30:55 -0800

    more groundwork

--- fastlabel/scripts/test_case.py
+++ fastlabel/scripts/test_case.py
@@ -2,3 +2,32 @@
 Replicate https://caseontology.org/examples/asgard using our bindings as closely
 as possible.
 """
+
+from pprint import pprint
+
+from fastlabel.uco import observable
+
+disk_facet = observable.FileFacet(
+    observableCreatedTime="2019-03-30T22:12:19.32Z",
+    extension="dd",
+    fileName="AsgardPD-2019033001-01.dd",
+    filePath="C:/evidence/AsgardPD-2019033001-01.dd",
+    isDirectory=False,
+    sizeInBytes=160080500
+)
+
+# TODO: apparently hasFacet is an iterable?? looks like there is some work to be
+# done here... i don't know when things should be considered lists or not, either
+# we have to edit the .ttl or change the logic used to determine iterable vs. scalar
+obj = observable.Image(
+    hasFacet=disk_facet
+)
+
+# TODO: handle @id generation
+# TODO: handle @type generation for non-standard JSON fields -- we can add/modify
+#       methods in our XMLSchema classes, or write a custom handler function, etc...
+
+# https://docs.pydantic.dev/latest/concepts/serialization/#serializing-with-duck-typing
+# Normally models are dumped recursively, but fields present in a subclass and not
+# in a parent class are not dumped by default. serialize_as_any must be passed.
+pprint(obj.model_dump(serialize_as_any=True))
