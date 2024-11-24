9e085dbc731e56515536844c2866c8ec4058a870
lgactna <lgonzalesna@gmail.com>
2024-11-23 21:04:56 -0800

    apparent xsd type support

---
+++ fastlabel/uco/co.py
@@ -0,0 +1,7 @@
+"""
+Manually created file that defines Collections Ontology types for UCO.
+
+This attempts to adhere to the Java implementations described at
+https://github.com/collections-ontology/collections-ontology
+as closely as possible.
+"""
