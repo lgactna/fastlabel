cb20765f34184da347acb3b658ac502f641dac3a
lgactna <lgonzalesna@gmail.com>
2024-11-24 15:15:42 -0800

    support for runtime referencing

--- fastlabel/scripts/test_case.py
+++ fastlabel/scripts/test_case.py
@@ -16,9 +16,6 @@ disk_facet = observable.FileFacet(
     sizeInBytes=160080500,
 )
 
-# TODO: apparently hasFacet is an iterable?? looks like there is some work to be
-# done here... i don't know when things should be considered lists or not, either
-# we have to edit the .ttl or change the logic used to determine iterable vs. scalar
 obj = observable.Image(hasFacet=disk_facet)
 
 # https://docs.pydantic.dev/latest/concepts/serialization/#serializing-with-duck-typing
@@ -26,12 +23,11 @@ obj = observable.Image(hasFacet=disk_facet)
 # in a parent class are not dumped by default. serialize_as_any must be passed.
 pprint(obj.model_dump(serialize_as_any=True))
 
-
-# TODO: honestly i don't know how IRIs work anymore lmao
 application_version = observable.ApplicationVersion(
     version="1.0",
 )
 application_facet = observable.ApplicationFacet(
-    installedVersionHistory=application_version
+    installedVersionHistory=[application_version.ref(), application_version]
 )
+
 pprint(application_facet.model_dump(serialize_as_any=True))
