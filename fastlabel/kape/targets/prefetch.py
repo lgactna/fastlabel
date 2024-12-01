c044bed18b6336845ea6a4880c6e5a95b2ad9385
lgactna <lgonzalesna@gmail.com>
2024-11-30 21:02:01 -0800

    working case output

--- fastlabel/kape/targets/prefetch.py
+++ fastlabel/kape/targets/prefetch.py
@@ -9,6 +9,8 @@ from typing import ClassVar, Type
 
 from fastlabel.kape.core import KapeModule, KapeTarget, KapeTargetConfiguration
 from fastlabel.kape.modules import programexecution
+from fastlabel.uco import observable
+from fastlabel.uco.core import UcoObject
 
 
 class EvidenceOfExecutionPrefetch0(KapeTarget):
@@ -72,6 +74,61 @@ class EvidenceOfExecution(KapeTargetConfiguration):
     ]
 
 
+def prefetch_output_entries_to_case(
+    output_entries: list[programexecution.PrefetchOutputEntry],
+) -> list[UcoObject]:
+    # This is the same between PrefetchPrefetch0 and PrefetchPrefetch1
+    observables: list[UcoObject] = []
+
+    for prefetch_entry in output_entries:
+        root = observable.WindowsPrefetch()
+
+        file_facet = observable.FileFacet(
+            isDirectory=False,
+            accessedTime=prefetch_entry.source_access_time,
+            observableCreatedTime=prefetch_entry.source_create_time,
+            modifiedTime=prefetch_entry.source_modify_time,
+            fileName=prefetch_entry.source_file.name,
+            filePath=str(prefetch_entry.source_file.parent),
+            extension=prefetch_entry.source_file.suffix.lstrip("."),
+        )
+
+        directory_objs: list[observable.ObservableObject] = []
+        for directory in prefetch_entry.directories:
+            directory_objs.append(
+                observable.File(
+                    hasFacet=[
+                        observable.FileFacet(isDirectory=True, fileName=str(directory))
+                    ]
+                )
+            )
+        file_objs: list[observable.ObservableObject] = []
+        for file in prefetch_entry.files_loaded:
+            file_objs.append(
+                observable.File(
+                    hasFacet=[
+                        observable.FileFacet(isDirectory=False, fileName=str(file))
+                    ]
+                )
+            )
+
+        # TODO: volume not supported because I'm not sure what that looks like
+        prefetch_facet = observable.WindowsPrefetchFacet(
+            accessedDirectory=directory_objs,
+            accessedFile=file_objs,
+            firstRun=sorted(prefetch_entry.run_times)[0],
+            lastRun=sorted(prefetch_entry.run_times)[-1],
+            timesExecuted=prefetch_entry.run_count,
+            applicationFileName=prefetch_entry.executable_name,
+            prefetchHash=prefetch_entry.hash,
+        )
+
+        root.hasFacet = [file_facet, prefetch_facet]
+        observables.append(root)
+
+    return observables
+
+
 class PrefetchPrefetch0(KapeTarget):
     name: ClassVar[str] = "Prefetch"
     base_path: ClassVar[Path] = Path("C:\\Windows\\prefetch\\")
@@ -80,6 +137,21 @@ class PrefetchPrefetch0(KapeTarget):
     associated_modules: ClassVar[list[Type[KapeModule]]] = [programexecution.PECmd]
 
     # Add any instance variables specific to this target as Pydantic fields here.
+    timeline_entries: list[programexecution.PrefetchTimelineEntry] = []
+    output_entries: list[programexecution.PrefetchOutputEntry] = []
+
+    @classmethod
+    def run_modules_on_module_dest(
+        cls, module_destination: Path
+    ) -> "PrefetchPrefetch0":
+        data = programexecution.PECmd.scan_module_destination(module_destination)
+        return cls(
+            timeline_entries=data.timeline_entries,
+            output_entries=data.output_entries,
+        )
+
+    def to_case_objects(self) -> list[UcoObject]:
+        return prefetch_output_entries_to_case(self.output_entries)
 
 
 class PrefetchPrefetch1(KapeTarget):
@@ -90,11 +162,21 @@ class PrefetchPrefetch1(KapeTarget):
     associated_modules: ClassVar[list[Type[KapeModule]]] = [programexecution.PECmd]
 
     # Add any instance variables specific to this target as Pydantic fields here.
+    timeline_entries: list[programexecution.PrefetchTimelineEntry] = []
+    output_entries: list[programexecution.PrefetchOutputEntry] = []
+
     @classmethod
-    def run_modules_on_target_dest(
-        self, target_destination: Path
+    def run_modules_on_module_dest(
+        cls, module_destination: Path
     ) -> "PrefetchPrefetch1":
-        return super().run_modules_on_target_dest(target_destination)
+        data = programexecution.PECmd.scan_module_destination(module_destination)
+        return cls(
+            timeline_entries=data.timeline_entries,
+            output_entries=data.output_entries,
+        )
+
+    def to_case_objects(self) -> list[UcoObject]:
+        return prefetch_output_entries_to_case(self.output_entries)
 
 
 class Prefetch(KapeTargetConfiguration):
